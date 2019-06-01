8bff6210849391a1d50ad47ffeaff31f1f9e2ee4
Leo21212 <leo.campagnollo@gmail.com>
2019-06-01 20:49:00 -0300

    Criando inimigos perseguidores

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject prefab;
+    public float repeatTime = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 2f, repeatTime);
+    }
+
+
+    void Spawn()
+    {
+        Instantiate(prefab, transform.position, Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
