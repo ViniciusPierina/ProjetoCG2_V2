e14502a5863c70e02f01443866471656784290e8
RenanManoel <renan_94almeida@yahoo.com.br>
2019-06-11 22:16:22 -0300

    Projeto Finalizado

--- Assets/Scripts/SkeletonCollider.cs
+++ Assets/Scripts/SkeletonCollider.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SkeletonCollider : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
         Destroy(this.gameObject);
-        Destroy(other.gameObject);
+
+        if (other.gameObject.name == "Soldadinho")
+            SceneManager.LoadScene("Assets/Scenes/Menu.unity", LoadSceneMode.Single);
     }
 }
---
+++ Assets/Scripts/SoldierCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoldierCollider : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "DGSK")
+        {
+            SceneManager.LoadScene("Assets\\Scenes\\Menu.unity", LoadSceneMode.Single);
+        }
+    }
+}
